4d837bc5a1f911e6dd5bf2d942db841459388b1a
NgocKim87 <64464003+NgocKim87@users.noreply.github.com>
2020-09-25 20:50:32 +0700

    fgg

--- GameCaroDeMo/Form1.cs
+++ GameCaroDeMo/Form1.cs
@@ -14,7 +14,7 @@ namespace GameCaroDeMo
 {
     public partial class Form1 : Form
     {
-        //partial trong public partial  lass
+        //partial trong public partial  
         #region Properties
         ChessBoardManager ChessBoard;
 
